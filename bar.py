40c39e1a4379dff6214543fcfa55f5530ec7850b
varshh26 <145096087+varshh26@users.noreply.github.com>
2023-11-03 11:24:19 +0530

    Add files via upload

---
+++ bar.py
@@ -0,0 +1,20 @@
+num=[9,6,2,16,14,81,18,8,8]
+gsum=sum(num)
+mean=gsum/n
+print("\n original list before sort:")
+	med sort()
+med1=num(n//2)
+med2=num(n//2-1)
+med3=(med1+med2)/2
+if num%2==0
+if(num(n//2))
+lk=[]
+i=0
+whilen>i
+gsum=i
+i=i+1fe
+
+print("\n original list after sort:")
+print("\n mean=",mean)
+print("\n median=",med)
+print("\n mode=",mode)
